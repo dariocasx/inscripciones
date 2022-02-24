83c6164ce75e194367b164c259ffd14c495d7bfb
dariocasx <58869926+dariocasx@users.noreply.github.com>
2022-02-24 11:32:06 -0300

    Add files via upload

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,6 @@
         <div class="row justify-content-between">
             <div class="col-sm-6 col-md-4 col-xl-3">
                 <div class="single-footer-widget footer_1">
-                    <a href="index.html"> <img src="{{ asset('img/logo.png') }}" alt=""> </a>
                     <p>UCA </p>
                     <p>Alumnos </p>
                 </div>
